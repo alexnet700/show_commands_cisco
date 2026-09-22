2a70e57f730df7af21d355858fc5a918b2b7114e
alexnet700 <alexbgh@myyahoo.com>
2026-09-22 00:23:08 +0000

    Add feature to connect to multiple switches

--- show_command.py
+++ show_command.py
@@ -14,10 +14,25 @@ SWACC02 = {
     'password': passwd
 }
 
-net_connect = ConnectHandler(**SWACC02)
-print(f"Connecting {SWACC02['host']}...")
-output = net_connect.send_command('show version', read_timeout = 60)
-print(output)
+SWDIST01 = {
+    'device_type': 'cisco_ios',
+    'host': '192.168.100.12',
+    'username': username,
+    'password': passwd
+}
 
-print("Closing connection...")
-net_connect.disconnect()
+SWCORE01 = {
+    'device_type': 'cisco_ios',
+    'host': '192.168.100.11',
+    'username': username,
+    'password': passwd
+}
+
+
+for device in (SWACC02, SWDIST01, SWCORE01):
+    print(f"\nConnecting {device['host']}...")
+    net_connect = ConnectHandler(**device)
+    output = net_connect.send_command('show version', read_timeout = 60)
+    print(output)
+    print(f"\nClosing connection {device['host']}...")
+    net_connect.disconnect()
