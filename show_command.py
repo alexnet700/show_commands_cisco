b6c7ca1c87fb2929ae56359d63c47119a5c203b3
alexnet700 <alexbgh@myyahoo.com>
2026-09-24 01:12:18 +0000

    Moved switches IP addresses in a separate file

--- show_command.py
+++ show_command.py
@@ -1,38 +1,55 @@
 from netmiko import ConnectHandler
 import os
 from dotenv import load_dotenv
+import sys
 
 load_dotenv()
 
+# Credentials username and password which are saved in .env
 username = os.environ.get('USER_NAME')
 passwd = os.environ.get('PASSWD')
 
-SWACC02 = {
-    'device_type': 'cisco_ios',
-    'host': '192.168.100.14',
-    'username': username,
-    'password': passwd
-}
+data = open("switches.txt", "r")
+list_of_data = []
 
-SWDIST01 = {
-    'device_type': 'cisco_ios',
-    'host': '192.168.100.12',
-    'username': username,
-    'password': passwd
-}
+def get_device_info(list_of_data, username, passwd):
+    devices = {
+        'device_type': 'cisco_ios',
+        'ip': 'list_of_data',
+        'username': username,
+        'password': passwd,
+    }
 
-SWCORE01 = {
-    'device_type': 'cisco_ios',
-    'host': '192.168.100.11',
-    'username': username,
-    'password': passwd
-}
+    return devices
 
+# Read the list of IP addresses in switches.txt
 
-for device in (SWACC02, SWDIST01, SWCORE01):
-    print(f"\nConnecting {device['host']}...")
-    net_connect = ConnectHandler(**device)
-    output = net_connect.send_command('show version', read_timeout = 60)
-    print(output)
-    print(f"\nClosing connection {device['host']}...")
-    net_connect.disconnect()
+def func_switch_list(delete_empty_lines=False):
+    print('Reading IP addresses from file')
+    with open('switches.txt', 'r') as file:
+        for line in file:
+            if delete_empty_lines and not line.strip():
+                continue
+            ip = line.strip()
+            list_of_data.append(ip)
+    print(f'Read {len(list_of_data)} IP addresses from file.')
+
+def func_show_command():
+    devices = get_device_info(list_of_data, username, passwd)
+    print('\x1b[1;31;47m' + "Start script, please do nothing" + '\x1b[0m')
+    for ip in list_of_data:
+        devices['ip'] = (ip)
+        ssh_connect = ConnectHandler(**devices)
+
+        output = ssh_connect.send_command('show version')
+
+        original_stdout = sys.stdout
+        with open('func_show command.txt', 'a+') as f:
+            sys.stdout = f
+            print(output)
+            sys.stdout = original_stdout
+
+        print('\x1b[6;30;42m' + " DONE " + '\x1b[0m')
+        ssh_connect.disconnect()
+func_switch_list(delete_empty_lines=True)
+func_show_command()
